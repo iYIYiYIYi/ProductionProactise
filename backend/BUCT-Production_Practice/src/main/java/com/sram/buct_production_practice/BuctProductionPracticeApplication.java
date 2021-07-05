7f5385f6b386c97f7c1eb49c6efdd1a75864de2c
Lagrange <45124583+mkbzas777@users.noreply.github.com>
2021-07-05 09:35:47 +0800

    init backend
    init

---
+++ backend/BUCT-Production_Practice/src/main/java/com/sram/buct_production_practice/BuctProductionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sram.buct_production_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuctProductionPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuctProductionPracticeApplication.class, args);
+    }
+
+}
