e297341743d35e7db37bf207d8dcfa2526f9deba
只猫 <1796861552@qq.com>
2021-07-05 10:05:22 +0800

    提交测试2

--- backend/BUCT-Production_Practice/src/main/java/com/sram/buct_production_practice/BuctProductionPracticeApplication.java
+++ backend/BUCT-Production_Practice/src/main/java/com/sram/buct_production_practice/BuctProductionPracticeApplication.java
@@ -8,6 +8,7 @@ public class BuctProductionPracticeApplication {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("test");
         SpringApplication.run(BuctProductionPracticeApplication.class, args);
     }
 
