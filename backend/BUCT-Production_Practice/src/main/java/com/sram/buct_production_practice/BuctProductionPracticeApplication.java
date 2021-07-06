4b661a531122ca9d588dd408835aa93f8e831e5a
只猫 <1796861552@qq.com>
2021-07-06 10:01:48 +0800

    初始数据爬取

--- backend/BUCT-Production_Practice/src/main/java/com/sram/buct_production_practice/BuctProductionPracticeApplication.java
+++ backend/BUCT-Production_Practice/src/main/java/com/sram/buct_production_practice/BuctProductionPracticeApplication.java
@@ -1,5 +1,6 @@
 package com.sram.buct_production_practice;
 
+import com.sram.buct_production_practice.controller.GetController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BuctProductionPracticeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BuctProductionPracticeApplication.class, args);
+//        SpringApplication.run(BuctProductionPracticeApplication.class, args);
+        GetController.getMethod();
     }
 
 }
