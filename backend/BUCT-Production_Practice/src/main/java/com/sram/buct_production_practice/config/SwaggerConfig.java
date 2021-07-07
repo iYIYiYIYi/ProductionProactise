6ee06deb8e25368031c5695dd9ebff5be79bff99
Lagrange <45124583+mkbzas777@users.noreply.github.com>
2021-07-07 10:06:29 +0800

    repair swagger  Add Error Response

--- backend/BUCT-Production_Practice/src/main/java/com/sram/buct_production_practice/config/SwaggerConfig.java
+++ backend/BUCT-Production_Practice/src/main/java/com/sram/buct_production_practice/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.sram.buct_production_practice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig{
 
     @Bean
     public Docket createRestApi(){
