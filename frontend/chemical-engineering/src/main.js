bcf1b32e3df3c798b3e1f12ad8bc44993d4688ba
SilverBullet1214 <80616600+SilverBullet1214@users.noreply.github.com>
2021-07-08 10:11:29 +0800

    new echarts

---
+++ frontend/chemical-engineering/src/main.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui' //element-ui的全部组件
+import 'element-ui/lib/theme-chalk/index.css'//element-ui的css
+import ECharts from 'vue-echarts'
+import { use } from 'echarts/core'
+
+// import ECharts modules manually to reduce bundle size
+import {
+  CanvasRenderer
+} from 'echarts/renderers'
+import {
+  BarChart
+} from 'echarts/charts'
+import {
+  GridComponent,
+  TooltipComponent
+} from 'echarts/components'
+
+use([
+  CanvasRenderer,
+  BarChart,
+  GridComponent,
+  TooltipComponent
+]);
+
+// register globally (or you can do it locally)
+Vue.component('v-chart', ECharts)
+Vue.config.productionTip = false
+Vue.use(ElementUI) //使用elementUI
+
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
