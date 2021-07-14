de58cd989f15b0bcc647bafb1006a4502023f31a
只猫 <1796861552@qq.com>
2021-07-14 09:30:26 +0800

    历史趋势波形频谱的整套流程

--- backend/BUCT-Production_Practice/src/main/java/com/sram/buct_production_practice/dao/TrendSpectrumValueDao.java
+++ backend/BUCT-Production_Practice/src/main/java/com/sram/buct_production_practice/dao/TrendSpectrumValueDao.java
@@ -3,6 +3,8 @@ package com.sram.buct_production_practice.dao;
 import com.sram.buct_production_practice.entity.TrendSpectrumValue;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigInteger;
+
 @Repository
 public interface TrendSpectrumValueDao {
     int deleteByPrimaryKey(Integer id);
@@ -18,4 +20,6 @@ public interface TrendSpectrumValueDao {
     int updateByPrimaryKey(TrendSpectrumValue record);
 
     TrendSpectrumValue selectByEquAndPoint(String equipmentUuid, String pointIdString);
+
+    TrendSpectrumValue selectByEquAndPointAndTime(String equipmentUuid, String pointIdString, BigInteger trendTime);
 }
--- backend/BUCT-Production_Practice/src/main/java/com/sram/buct_production_practice/dao/TrendValueDao.java
+++ backend/BUCT-Production_Practice/src/main/java/com/sram/buct_production_practice/dao/TrendValueDao.java
@@ -3,6 +3,8 @@ package com.sram.buct_production_practice.dao;
 import com.sram.buct_production_practice.entity.TrendValue;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigInteger;
+
 @Repository
 public interface TrendValueDao {
     int deleteByPrimaryKey(Integer id);
@@ -18,4 +20,7 @@ public interface TrendValueDao {
     int updateByPrimaryKey(TrendValue record);
 
     TrendValue selectByEquAndPoint(String equipmentUuid, String pointIdString);
+
+    TrendValue selectByEquAndPointAndTime(String equipmentUuid, String pointIdString, BigInteger trendTime);
+
 }
